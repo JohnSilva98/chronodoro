3fbf19e937f84e9fbee50b5b836ac7b0975b8f85
JohnSilva98 <jonathan.design16@outlook.com>
2026-03-24 11:24:55 -0300

    feat: creating new task and using setState to save it on the state

--- app/components/MainForm.tsx
+++ app/components/MainForm.tsx
@@ -1,23 +1,26 @@
 import { Cicles } from "./Cicles"
 import Toastify from 'toastify-js'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { CirclePlay, CircleStop } from 'lucide-react'
 import { useTaskContext } from "../contexts/TaskContext/useTaskContext"
+import { TaskModel } from "../models/TaskModel"
 
 
 export default function MainForm() {
  
+const {setState} = useTaskContext()
 
- const [taskName, setTaskName] = useState('')
- const [started, setStarted] = useState(false)
- const [cicle, setCicle] = useState('descanse')
-  const [focusTime, setFocusTime] = useState(25)
-  const [shortBreakTime, setShortBreakTime] = useState(5)
-  const [longBreakTime, setLongBreakTime] = useState(15)
 
- const handleStart = (e: { preventDefault: () => void }) => {
-     e.preventDefault()
-      if(taskName === '') {
+ 
+
+  const TaskNameInput = useRef<HTMLInputElement>(null)
+  function handleCreateNewTask(e: React.FormEvent){
+    e.preventDefault();
+    
+    if(TaskNameInput.current === null) return
+    const taskName = TaskNameInput.current.value.trim()
+
+    if(!taskName) {
       Toastify({text: 'Por favor, insira uma tarefa', duration: 3000,
         close: true, //allow close the toast
         gravity: "top", // `top` or `bottom`
@@ -30,28 +33,37 @@ export default function MainForm() {
       }).showToast();
       return;
     }
-    if(started === true) {
-      setStarted(false)
-      setTaskName('')
-      
-    } else {
-      setStarted(true)
-    }
-    
-    
-   
-  }
 
-  function handleCreateNewTask(e: React.FormEvent){
-    e.preventDefault();
-    
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      CompleteDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime'
+    }
+
+    const secondsRemaining = newTask.duration * 60;
+    setState(prevState => {
+      return{
+        ...prevState,
+        config: {...prevState.config},
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSeconds: '00:00',
+        tasks:[...prevState.tasks, newTask],
+        
+      }
+    })
     
   }
 
   return (
  <form onSubmit={handleCreateNewTask} className="flex flex-col items-center justify-center mt-2">
         <label className='text-(--text-primary) mb-4'>Task:</label>
-        <input onFocus={()=> {"border-(--primary-green-light)"}} onChange={(e)=> setTaskName(e.target.value)} className='text-(--text-primary) border-b-2 border-(--border-light) p-2 rounded-sm' type="text" id="task" name="task" value={taskName} />
+        <input ref={TaskNameInput} onFocus={()=> {"border-(--primary-green-light)"}}  className='text-(--text-primary) border-b-2 border-(--border-light) p-2 rounded-sm' type="text" id="task" name="task"  />
         <div className="flex items-center gap-2 mt-3">
           <p className='text-(--text-primary)'>Nesse ciclo</p>
           <p className='text-(--text-primary)'> <span className='text-(--primary-green-light) font-semibold'>{cicle}</span></p>
@@ -59,7 +71,7 @@ export default function MainForm() {
           <p className='text-(--text-primary)'> <span className='text-(--primary-green-light) font-semibold'>{shortBreakTime} minutos</span></p>
         </div>
         <Cicles />
-        <button  type="submit" className={`text-[var(--text-primary)] hover:bg-[var(--primary-green-hover)] duration-300 border border-[var(--border-dark)] p-2 rounded-lg mt-2 mb-4 cursor-pointer px-8 ${started ? 'bg-[var(--error)] hover:bg-[var(--error)]/80' : 'bg-[var(--success)] hover:bg-[var(--success)]/80'}`}>{started ? <CircleStop size={24} color="white" /> : <CirclePlay size={24} color="white" />}</button>
+        <button  type="submit" className={`text-(--text-primary) hover:bg-(--primary-green-hover) duration-300 border border-(--border-dark) p-2 rounded-lg mt-2 mb-4 cursor-pointer px-8 ${started ? 'bg-(--error) hover:bg-(--error)/80' : 'bg-(--success) hover:bg-(--success)/80'}`}>{started ? <CircleStop size={24} color="white" /> : <CirclePlay size={24} color="white" />}</button>
        </form>
        
   )
