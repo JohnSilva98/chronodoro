33eb1e2e8bb0b7ee6dc799e7e89d9a8d0a743cf8
JohnSilva98 <jonathan.design16@outlook.com>
2026-03-24 16:41:39 -0300

    criando worker

--- app/components/MainForm.tsx
+++ app/components/MainForm.tsx
@@ -62,8 +62,12 @@ const tipsForWhenNoActiveTask = {
 
     dispatch({type: TaskActionsTypes.START_TASK, payload: newTask})
 
-    
+    const worker = new Worker(new URL('../workers/TimerWorker.js', import.meta.url))
+    worker.postMessage("teste")
+
+
   }
+
 
 function handleInterruptTask() {
   dispatch({type: TaskActionsTypes.INTERRUPT_TASK })
---
+++ app/workers/TimerWorker.js
@@ -0,0 +1,3 @@
+self.onmessage = function(e) {
+    console.log('worker recebeu', e.data);
+}
